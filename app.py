89b78fac1d0007bd14f86ba9e851361c7e7da96c
Raffli Bisma Aryasatya <154855593+Raffli-Dev@users.noreply.github.com>
2024-07-01 17:44:00 +0700

    Ada Best Practice data API

--- app.py
+++ app.py
@@ -1,53 +1,79 @@
-#integration API Data
-from flask import Flask,request,jsonify
-from pymongo import MongoClient
+from flask import Flask, request, jsonify
 
-client = MongoClient("mongodb://latihan:caplin11@ac-s43qydu-shard-00-00.xrmw0we.mongodb.net:27017,ac-s43qydu-shard-00-01.xrmw0we.mongodb.net:27017,ac-s43qydu-shard-00-02.xrmw0we.mongodb.net:27017/?ssl=true&replicaSet=atlas-b6z32o-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Latihan
-db = client.api_integration
+app = Flask(__name__)
 
-app=Flask(__name__)
-@app.route('/get-user/<id>')
-def home(id):
-    user_data = [{
-        "data": [
-            {
-                "user_id": id,
-                "nama": "raffli",
-                "umur": "21"
-            },
-            {
-                "user_id": id,
-                "nama": "GG",
-                "umur": "21"
-            }
-        ]
-    },
+@app.route('/get-user/<id>', methods=['GET'])
+def get_user(id):
+    user_data = [
+        {
+            "data": [
+                {
+                    "user_id": id,
+                    "nama": "raffli",
+                    "umur": "21"
+                },
+                {
+                    "user_id": id,
+                    "nama": "GG",
+                    "umur": "21"
+                }
+            ]
+        },
         {
             "sugestion": [
-            {
-                "Alamat": "Magetan",
-                "Nomer_Hp": "085730183893",
-                "Propinsi": "Jawa Timur"
-            },
-            {
-                "Alamat": "Madiun",
-                "Nomer_Hp": "085730183893",
-                "Propinsi": "Jawa Timur"
-            },
-            {
-                "Alamat": "Magetan",
-                "Nomer_Hp": "085730183893",
-                "Propinsi": "Jawa Timur"
-            }
-        ]
-        }]
-    
-    db.api.insert_one(user_data)
+                {
+                    "Alamat": "Magetan",
+                    "Nomer_Hp": "085730183893",
+                    "Propinsi": "Jawa Timur"
+                },
+                {
+                    "Alamat": "Madiun",
+                    "Nomer_Hp": "085730183893",
+                    "Propinsi": "Jawa Timur"
+                },
+                {
+                    "Alamat": "Magetan",
+                    "Nomer_Hp": "085730183893",
+                    "Propinsi": "Jawa Timur"
+                }
+            ]
+        }
+    ]
+
     test = request.args.get("test")
     if test:
         user_data[0]["test"] = test
 
     return jsonify(user_data), 200
+
+#Best Practice API
+@app.route('/practice/<id>', methods=['GET'])
+def practice(id):
+    user_data = [
+        {
+            "code": "200",
+            "status": "Status OK!!!",
+            "data": {
+                "Name": "Hello World",
+                "age": 21,
+                "Address": "Magetan City"
+            },
+            "errors": {
+                "id": [
+                    "Product Error"
+                ]
+            }
+        }
+    ]
+
+    test = request.args.get("test")
+    if test:
+        user_data[0]["test"] = test
+
+    return jsonify(user_data), 200
+
+
+
 
 @app.route('/create-user', methods=['POST'])
 def create_user():
@@ -55,4 +81,4 @@ def create_user():
     return jsonify(data), 201
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
